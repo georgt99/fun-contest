404518ee2409240e8386fef67eb4ce7c1b56f4dd
Otto Kissig <1622854+EightSQ@users.noreply.github.com>
2021-07-22 00:47:13 +0200

    [meow] Add otto solution

---
+++ cp21/fun-contest/meow/executables/solution-otto.cpp
@@ -0,0 +1,64 @@
+
+//region template{{{
+//https://github.com/schmidtjonas/CPT/blob/master/template.cpp
+#include <bits/stdc++.h>
+
+#define GET_MACRO(_1,_2,_3,NAME,...) NAME
+#define rep(...)        GET_MACRO(__VA_ARGS__, repn, rep0)(__VA_ARGS__)
+#define rep0(i, b)      for(int i = 0; i < (b); ++i)
+#define repn(i, a, b)   for(int i = (a); i < (b); ++i)
+#define trav(a, b)      for(auto& a : b)
+#define all(a)          (a).begin(),(a).end()
+#define endl            '\n'
+#define sz(x)           ((int)(x).size())
+#define tests           int ___;cin>>___;while(___--)
+
+using namespace std;
+using ll = int64_t;
+using ld = long double;
+using ul = uint64_t;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+using vpi = vector<pi>;
+
+static constexpr int inf = (int)1e9 + 5;
+static constexpr ll infl = (ll)1e18 + 5;
+static mt19937 rng((uint32_t)chrono::duration_cast<chrono::nanoseconds>(chrono::high_resolution_clock::now().time_since_epoch()).count());
+//endregion}}}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    tests {
+        string s; cin >> s;
+        ll n = s.size() - 2;
+        ll i = 1;
+        ll j = 2;
+        ll su = 3;
+        ll res = 0;
+        while (true) {
+            if (su == n) {
+                res++;
+                su += ++j;
+            } else if (su < n) {
+                su += ++j;
+            } else if (su > n) {
+                if (j - i + 1 == 2) {
+                    break;
+                } else {
+                    su -= i++;
+                }
+            }
+        }
+        cout << res << endl;
+    }
+
+    return 0;
+}
+
